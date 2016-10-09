f34a543c6223e677ed0b460971c1a2c09df69e6b
SaskiaPuspita <dewsaski@gmail.com>
2016-10-09 10:13:02 +0700

    Livecycle Music.

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.learn.activitylivecycle;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LivecycleTag";
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //installing awal apk
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: ");
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
 
     }
 
@@ -28,12 +31,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume: ");
+        mediaPlayer.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause: ");
+        mediaPlayer.pause();
     }
 
     @Override
@@ -53,5 +58,8 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
+        if (mediaPlayer != null)
+            mediaPlayer.release();
     }
+
 }
